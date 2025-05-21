330219bbf7eb4166389975b158fef4796925d79d
illusion278 <dimarik.koreibo@gmail.com>
2025-05-21 20:23:14 +1000

    Add Jenkinsfile for CI/CD pipeline

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
-// main.cpp
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 int main() {
-    setlocale(LC_ALL, "RUS");
-    srand(time(0));
-    int secret = rand() % 100 + 1;
+    std::srand(std::time(0));
+    int secretNumber = std::rand() % 100 + 1;
     int guess = 0;
 
     std::cout << "Угадай число от 1 до 100!\n";
 
-    while (guess != secret) {
+    while (guess != secretNumber) {
         std::cout << "Твоя догадка: ";
-        std::cin >> guess;
+        std::cin >> guess;  // Важно: проверьте, что ввод работает
 
-        if (guess > secret) std::cout << "Слишком много!\n";
-        else if (guess < secret) std::cout << "Слишком мало!\n";
+        if (guess < secretNumber) {
+            std::cout << "Слишком мало!\n";
+        }
+        else if (guess > secretNumber) {
+            std::cout << "Слишком много!\n";
+        }
+        else {
+            std::cout << "Правильно! Это " << secretNumber << "!\n";
+        }
     }
 
-    std::cout << "Поздравляю! Ты угадал число " << secret << "!\n";
     return 0;
 }
